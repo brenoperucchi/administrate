65341e8c02ae510b6ad8cb5fb82e3626f571a80a
Breno Perucchi <bperucchi@gmail.com>
2024-05-12 03:32:11 -0300

    Reapply "Update number.rb"
    This reverts commit e1fc88e9c6772e5a9488a92e97158f35e8ec99e2.

--- lib/administrate/field/number.rb
+++ lib/administrate/field/number.rb
@@ -5,9 +5,7 @@ module Administrate
   module Field
     class Number < Field::Base
       def to_s
-        data = if data.class.to_s == "String"
-                 data.empty? ? nil : data.to_i
-               end
+        data = data.empty? ? nil : data.to_i if data.class.to_s == "String"
         result = data.nil? ? "-" : format_string % value
         result = format(result) if options[:format]
         prefix + result + suffix
