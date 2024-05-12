4ff6121ae2b0a104a5b3632936b4423ab8d48a0d
Breno Perucchi <bperucchi@gmail.com>
2024-05-12 05:02:11 -0300

    Update number.rb

--- lib/administrate/field/number.rb
+++ lib/administrate/field/number.rb
@@ -5,7 +5,6 @@ module Administrate
   module Field
     class Number < Field::Base
       def to_s
-        data = data.empty? ? nil : data.to_i if data.class.to_s == "String"
         result = data.nil? ? "-" : format_string % value
         result = format(result) if options[:format]
         prefix + result + suffix
